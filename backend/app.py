4549ecdc49e7de8e86b142d6c2bf38fbd427a7f8
Marta <marciajdrs19@gmail.com>
2026-06-24 13:59:16 +0200

    gunicorn

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_cors import CORS
 
@@ -29,4 +31,5 @@ def create_app():
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
